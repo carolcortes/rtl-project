5c8db78f51c10d5900c594ef306f7706be8731bd
carolcortes <caroline.ocortes@gmail.com>
2022-07-18 15:28:53 -0300

    iniciando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from '../App';
+
+describe('Testes do componente App.js', () => {
+  it('Verifica se topo da página contém um conjunto fixo de links de navegação', () => {
+    // Testes
+  });
+});
